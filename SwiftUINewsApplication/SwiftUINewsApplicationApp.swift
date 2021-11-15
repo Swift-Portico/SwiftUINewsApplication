3d791234a66927b0de969f386b8ed9f1ea969ed7
pradeepiOS <pradeep.iostech@gmail.com>
2021-11-15 09:09:14 +0530

    Initial Commit

---
+++ SwiftUINewsApplication/SwiftUINewsApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINewsApplicationApp.swift
+//  SwiftUINewsApplication
+//
+//  Created by Pradeep's Macbook on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINewsApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
